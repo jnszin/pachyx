24972a29b0e0cce9a71b03ab2a08bcaf08ca7131
jnszin <jonasalvespacheco@gmail.com>
2026-08-23 18:19:20 -0300

    feat: integração formulários com prisma, novo faq e remoção do azul

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,19 @@
-import { PrismaClient } from '@prisma/client'
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@prisma/client';
 
 const prismaClientSingleton = () => {
-  return new PrismaClient()
-}
+  const pool = new Pool({ connectionString: process.env.DATABASE_URL as string });
+  const adapter = new PrismaPg(pool);
+  return new PrismaClient({ adapter });
+};
 
-declare global {
-  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>
-}
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
-export default prisma
+export default prisma;
 
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;
